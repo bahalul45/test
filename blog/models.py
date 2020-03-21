30d9c7ca5905cea281e29c21cb1dad69a70883ca
ijharislam <ijharislam@gmail.com>
2020-03-21 09:34:46 +0600

    Add Image Field

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+	image = models.ImageField(null=True, blank=True)
 	title = models.CharField(max_length=1000)
 	author = models.CharField(max_length=100)
 	datetime = models.DateTimeField(auto_now=True)
